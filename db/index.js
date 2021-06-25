1d5bfb4dbede2ad121d765147ad47dea74e02f65
关东煮小熊仔 <49669775+Axiano@users.noreply.github.com>
2021-06-25 19:21:47 +0800

    Update index.js

--- db/index.js
+++ db/index.js
@@ -3,10 +3,10 @@ const mysql = require('mysql')
 
 // 创建数据库连接对象
 const db = mysql.createPool({
-  host: '1.15.236.39',
-  user: 'myblog',
-  password: 'fxsBEdBfk5BMxGtd',
-  database: 'myblog',
+  host: '127.0.0.1',
+  user: '用户名',
+  password: '密码',
+  database: '数据库名',
   charset: 'utf8mb4'
 })
 
